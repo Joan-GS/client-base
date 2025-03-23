a5f58d630772848492a4b8848d90f0372a108464
Joan <jgose2001@gmail.com>
2025-03-23 23:27:39 +0100

    1a version kilter board

---
+++ src/screens/create-climbs/components/CreateClimbForm.tsx
@@ -0,0 +1,236 @@
+import React, { useState, useCallback } from "react";
+import { SafeAreaView } from "@/src/components/ui/safe-area-view";
+import { Input, InputField } from "@/src/components/ui/input";
+import { Button, ButtonText } from "@/src/components/ui/button";
+import { TouchableOpacity, ScrollView } from "react-native";
+import { router } from "expo-router";
+import { DashboardLayout } from "../../dashboard/dashboard-layout";
+import { createClimb, CreateClimbRequest } from "../api/createClimbs";
+import { useForm, Controller } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useToast, Toast, ToastTitle } from "@/src/components/ui/toast";
+import { useTranslation } from "react-i18next";
+
+import {
+  Container,
+  Title,
+  ErrorText,
+  Label,
+  TagsContainer,
+  TagBadge,
+  GradeBadge,
+  StatusBadge,
+  StyledTagsContainer,
+} from "../styles";
+
+import { CLIMBING_GRADE, STATUS, KILTER_TAGS } from "@joan16/shared-base";
+import { BadgeText, useMediaQuery, VStack } from "@gluestack-ui/themed";
+
+// Validation schema
+const climbSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, "* The title is too short")
+    .required("* Title is required"),
+  description: Yup.string().optional(),
+  grade: Yup.string().required("* Grade is required"),
+  tags: Yup.array().max(3, "Maximum 3 tags"),
+});
+
+export const CreateClimbForm = () => {
+  const [selectedTags, setSelectedTags] = useState<KILTER_TAGS[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<STATUS>(STATUS.PUBLIC);
+  const { t } = useTranslation();
+  const toast = useToast();
+  const [isMediumScreen] = useMediaQuery({ maxWidth: 768 });
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(climbSchema),
+    defaultValues: { title: "", description: "", grade: "", tags: [] },
+  });
+
+  // Function to toggle tags selection
+  const toggleTag = useCallback(
+    (tag: KILTER_TAGS) => {
+      setSelectedTags((prevTags) =>
+        prevTags.includes(tag)
+          ? prevTags.filter((t) => t !== tag)
+          : prevTags.length < 3
+          ? [...prevTags, tag]
+          : prevTags
+      );
+    },
+    [setSelectedTags]
+  );
+
+  // Function to show toast messages
+  const showToast = useCallback(
+    (message: string, type: "success" | "error") => {
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <Toast nativeID={id} action={type}>
+            <ToastTitle>{t(message)}</ToastTitle>
+          </Toast>
+        ),
+      });
+    },
+    [toast, t]
+  );
+
+  // Handle form submission
+  const onSubmit = async (data: any) => {
+    try {
+      const climbData: CreateClimbRequest = {
+        title: data.title,
+        description: data.description || "",
+        ratingAverage: 0,
+        grade: data.grade,
+        gradeAverage: data.grade,
+        tags: selectedTags.map(String),
+        status: selectedStatus,
+        createdBy: "67de80abfdf66272446aad0e",
+      };
+
+      await createClimb(climbData);
+
+      showToast("The climb was created successfully!", "success");
+
+      reset();
+      setSelectedTags([]);
+      setSelectedStatus(STATUS.PUBLIC);
+      router.replace("/dashboard/dashboard-layout");
+    } catch (error) {
+      showToast("Could not create the climb", "error");
+    }
+  };
+
+  // Handle form validation errors
+  const onError = () =>
+    showToast("Please check the form fields and try again.", "error");
+
+  return (
+    <ScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ paddingBottom: 20 }}
+    >
+      <Container>
+        {isMediumScreen && <Title>{t("Set Boulder")}</Title>}{" "}
+        {/* Title Input */}
+        <Controller
+          name="title"
+          control={control}
+          render={({ field }) => (
+            <Input isInvalid={!!errors.title}>
+              <InputField
+                placeholder={t("Title")}
+                value={field.value}
+                onChangeText={field.onChange}
+              />
+            </Input>
+          )}
+        />
+        {errors.title && <ErrorText>{errors.title.message}</ErrorText>}
+        {/* Description Input */}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <Input>
+              <InputField
+                placeholder={t("Description")}
+                value={field.value}
+                onChangeText={field.onChange}
+              />
+            </Input>
+          )}
+        />
+        {/* Grade Selection */}
+        <VStack>
+          <Label>{t("Select Grade")}:</Label>
+          <StyledTagsContainer>
+            {Object.values(CLIMBING_GRADE).map((grade) => (
+              <Controller
+                key={grade}
+                name="grade"
+                control={control}
+                render={({ field }) => (
+                  <TouchableOpacity onPress={() => field.onChange(grade)}>
+                    <GradeBadge selected={field.value === grade}>
+                      <BadgeText>{grade}</BadgeText>
+                    </GradeBadge>
+                  </TouchableOpacity>
+                )}
+              />
+            ))}
+          </StyledTagsContainer>
+          {errors.grade && <ErrorText>{errors.grade.message}</ErrorText>}
+        </VStack>
+        {/* Tag Selection */}
+        <VStack>
+          <Label>{t("Select up to 3 tags")}:</Label>
+          <TagsContainer>
+            {Object.values(KILTER_TAGS).map((tag) => (
+              <TouchableOpacity key={tag} onPress={() => toggleTag(tag)}>
+                <TagBadge
+                  style={{
+                    backgroundColor: selectedTags.includes(tag)
+                      ? "#4CAF50"
+                      : "#E0E0E0",
+                  }}
+                >
+                  <BadgeText>{tag}</BadgeText>
+                </TagBadge>
+              </TouchableOpacity>
+            ))}
+          </TagsContainer>
+        </VStack>
+        {/* Status Selection */}
+        <VStack>
+          <Label>{t("Select Status")}:</Label>
+          <TagsContainer>
+            {Object.values(STATUS).map((status) => (
+              <TouchableOpacity
+                key={status}
+                onPress={() => setSelectedStatus(status)}
+              >
+                <StatusBadge
+                  style={{
+                    backgroundColor:
+                      selectedStatus === status
+                        ? status === STATUS.PUBLIC
+                          ? "green"
+                          : status === STATUS.DRAFT
+                          ? "gray"
+                          : "red"
+                        : "#E0E0E0",
+                  }}
+                >
+                  <BadgeText>{t(status)}</BadgeText>
+                </StatusBadge>
+              </TouchableOpacity>
+            ))}
+          </TagsContainer>
+        </VStack>
+        {/* Submit Button */}
+        <VStack style={{ alignItems: "center", width: "100%", marginTop: 40 }}>
+          <Button
+            onPress={handleSubmit(onSubmit, onError)}
+            isDisabled={isSubmitting}
+          >
+            <ButtonText>
+              {isSubmitting ? t("Creating...") : t("Set Boulder")}
+            </ButtonText>
+          </Button>
+        </VStack>
+      </Container>
+    </ScrollView>
+  );
+};
+
+
--- src/screens/create-climbs/index.tsx
+++ src/screens/create-climbs/index.tsx
@@ -1,239 +1,168 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
+import Svg, { G, Circle } from "react-native-svg";
+import { View, ScrollView } from "react-native";
 import { SafeAreaView } from "@/src/components/ui/safe-area-view";
-import { Input, InputField } from "@/src/components/ui/input";
-import { Button, ButtonText } from "@/src/components/ui/button";
-import { TouchableOpacity, ScrollView } from "react-native";
-import { router } from "expo-router";
-import { DashboardLayout } from "../dashboard/dashboard-layout";
-import { createClimb, CreateClimbRequest } from "./api/createClimbs";
-import { useForm, Controller } from "react-hook-form";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useToast, Toast, ToastTitle } from "@/src/components/ui/toast";
 import { useTranslation } from "react-i18next";
+import { DashboardLayout } from "../dashboard/dashboard-layout";
+import { BigHoldsSvg } from "./components/BigHoldsSvg";
+import { SmallHoldsSvg } from "./components/SmallHoldsSvg";
+import { holdsData } from "./holds/holds";
 
-import {
-  Container,
-  Title,
-  ErrorText,
-  Label,
-  TagsContainer,
-  TagBadge,
-  GradeBadge,
-  StatusBadge,
-  StyledTagsContainer,
-} from "./styles";
+const COORDINATE_MULTIPLIER = 7.5;
+const DELTA_X = 0;
+const DELTA_Y = 1170;
 
-import { CLIMBING_GRADE, STATUS, KILTER_TAGS } from "@joan16/shared-base";
-import { BadgeText, useMediaQuery, VStack } from "@gluestack-ui/themed";
+const PACKET_MIDDLE = 81;
+const PACKET_FIRST = 82;
+const PACKET_LAST = 83;
+const PACKET_ONLY = 84;
+const MESSAGE_BODY_MAX_LENGTH = 255;
 
-// Validation schema
-const climbSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(3, "* The title is too short")
-    .required("* Title is required"),
-  description: Yup.string().optional(),
-  grade: Yup.string().required("* Grade is required"),
-  tags: Yup.array().max(3, "Maximum 3 tags"),
-});
 
-const CreateClimbScreen = () => {
-  const [selectedTags, setSelectedTags] = useState<KILTER_TAGS[]>([]);
-  const [selectedStatus, setSelectedStatus] = useState<STATUS>(STATUS.PUBLIC);
-  const { t } = useTranslation();
-  const toast = useToast();
-  const [isMediumScreen] = useMediaQuery({ maxWidth: 768 });
+const colors: Record<string, string> = {
+  transparent: "FFFFFF",
+  blue: "4CF0FD",
+  green: "00FF00",
+  yellow: "FBE400",
+  magenta: "FF00FF",
+};
+const colorKeys = Object.keys(colors);
 
-  const {
-    control,
-    handleSubmit,
-    reset,
-    formState: { errors, isSubmitting },
-  } = useForm({
-    resolver: yupResolver(climbSchema),
-    defaultValues: { title: "", description: "", grade: "", tags: [] },
+const encodeColor = (hex: string): number => {
+  const r = parseInt(hex.substring(0, 2), 16) / 32;
+  const g = parseInt(hex.substring(2, 4), 16) / 32;
+  const b = parseInt(hex.substring(4, 6), 16) / 64;
+  return (r << 5) | (g << 2) | b;
+};
+
+const checksum = (data: number[]): number => {
+  let sum = data.reduce((acc: number, val: number) => (acc + val) & 255, 0);
+  return ~sum & 255;
+};
+
+const wrapBytes = (data: number[]): number[] => {
+  if (data.length > MESSAGE_BODY_MAX_LENGTH) return [];
+  return [1, data.length, checksum(data), 2, ...data, 3];
+};
+
+interface HoldWithColor {
+  position: number;
+  color: string;
+}
+
+const prepBytesV3 = (holds: HoldWithColor[]): number[] => {
+  let packets: number[][] = [];
+  let currentPacket: number[] = [PACKET_MIDDLE];
+
+  holds.forEach(({ position, color }) => {
+    if (currentPacket.length + 3 > MESSAGE_BODY_MAX_LENGTH) {
+      packets.push(currentPacket);
+      currentPacket = [PACKET_MIDDLE];
+    }
+    currentPacket.push(
+      position & 255,
+      (position >> 8) & 255,
+      encodeColor(colors[color])
+    );
   });
 
-  // Function to toggle tags selection
-  const toggleTag = useCallback(
-    (tag: KILTER_TAGS) => {
-      setSelectedTags((prevTags) =>
-        prevTags.includes(tag)
-          ? prevTags.filter((t) => t !== tag)
-          : prevTags.length < 3
-          ? [...prevTags, tag]
-          : prevTags
+  packets.push(currentPacket);
+
+  if (packets.length === 1) packets[0][0] = PACKET_ONLY;
+  else {
+    packets[0][0] = PACKET_FIRST;
+    packets[packets.length - 1][0] = PACKET_LAST;
+  }
+
+  return packets.flatMap(wrapBytes);
+};
+
+export const CreateClimbScreen: React.FC = () => {
+  const { t } = useTranslation();
+  const [selectedHolds, setSelectedHolds] = useState<Record<string, string>>(
+    {}
+  );
+
+  const toggleHold = (id: string, position: number) => {
+    setSelectedHolds((prev) => {
+      const currentColorIndex = colorKeys.indexOf(prev[id] || "transparent");
+      const newColor = colorKeys[(currentColorIndex + 1) % colorKeys.length];
+      const updatedHolds = { ...prev, [id]: newColor };
+
+      const bluetoothCode = prepBytesV3(
+        Object.entries(updatedHolds)
+          .filter(([_, color]) => color !== "transparent")
+          .map(([holdId, color]) => {
+            const hold = holdsData.find((h) => h.id === holdId);
+            return hold ? { position: hold.position, color } : null;
+          })
+          .filter((h): h is HoldWithColor => h !== null)
       );
-    },
-    [setSelectedTags]
-  );
 
-  // Function to show toast messages
-  const showToast = useCallback(
-    (message: string, type: "success" | "error") => {
-      toast.show({
-        placement: "top",
-        render: ({ id }) => (
-          <Toast nativeID={id} action={type}>
-            <ToastTitle>{t(message)}</ToastTitle>
-          </Toast>
-        ),
-      });
-    },
-    [toast, t]
-  );
+      console.log(
+        "Código Bluetooth:",
+        bluetoothCode
+          .map((byte) => byte.toString(16).padStart(2, "0"))
+          .join(" ")
+      );
 
-  // Handle form submission
-  const onSubmit = async (data: any) => {
-    try {
-      const climbData: CreateClimbRequest = {
-        title: data.title,
-        description: data.description || "",
-        ratingAverage: 0,
-        grade: data.grade,
-        gradeAverage: data.grade,
-        tags: selectedTags.map(String),
-        status: selectedStatus,
-        createdBy: "67de80abfdf66272446aad0e",
-      };
-
-      await createClimb(climbData);
-
-      showToast("The climb was created successfully!", "success");
-
-      reset();
-      setSelectedTags([]);
-      setSelectedStatus(STATUS.PUBLIC);
-      router.replace("/dashboard/dashboard-layout");
-    } catch (error) {
-      showToast("Could not create the climb", "error");
-    }
+      return updatedHolds;
+    });
   };
 
-  // Handle form validation errors
-  const onError = () =>
-    showToast("Please check the form fields and try again.", "error");
-
   return (
-    <ScrollView
-      style={{ flex: 1 }}
-      contentContainerStyle={{ paddingBottom: 20 }}
-    >
-      <Container>
-        {isMediumScreen && <Title>{t("Set Boulder")}</Title>}{" "}
-        {/* Title Input */}
-        <Controller
-          name="title"
-          control={control}
-          render={({ field }) => (
-            <Input isInvalid={!!errors.title}>
-              <InputField
-                placeholder={t("Title")}
-                value={field.value}
-                onChangeText={field.onChange}
+    <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+      <View style={{ alignItems: "center", paddingTop: 20 }}>
+        <Svg
+          width={"100%"}
+          height={720}
+          viewBox="0 0 1080 1170"
+          pointerEvents="box-none"
+        >
+          <G>
+            <BigHoldsSvg width="100%" height="100%" />
+          </G>
+          <G>
+            <SmallHoldsSvg width="100%" height="100%" />
+          </G>
+          {holdsData.map(({ id, position, x, y }) => {
+            const selectedColor = selectedHolds[id];
+            const hexColor = colors[selectedColor] || "FFFFFF"; // Color en formato hex
+
+            const r = parseInt(hexColor.substring(0, 2), 16);
+            const g = parseInt(hexColor.substring(2, 4), 16);
+            const b = parseInt(hexColor.substring(4, 6), 16);
+
+            return (
+              <Circle
+                key={id}
+                cx={x * COORDINATE_MULTIPLIER + DELTA_X}
+                cy={-y * COORDINATE_MULTIPLIER + DELTA_Y}
+                r={35}
+                fill={
+                  selectedColor && selectedColor !== "transparent"
+                    ? `rgba(${r}, ${g}, ${b}, 0.2)`
+                    : "transparent"
+                }
+                stroke={
+                  selectedColor && selectedColor !== "transparent"
+                    ? `rgb(${r}, ${g}, ${b})`
+                    : "transparent"
+                }
+                strokeWidth={
+                  selectedColor && selectedColor !== "transparent" ? 3 : 0
+                }
+                onPress={() => toggleHold(id, position)}
               />
-            </Input>
-          )}
-        />
-        {errors.title && <ErrorText>{errors.title.message}</ErrorText>}
-        {/* Description Input */}
-        <Controller
-          name="description"
-          control={control}
-          render={({ field }) => (
-            <Input>
-              <InputField
-                placeholder={t("Description")}
-                value={field.value}
-                onChangeText={field.onChange}
-              />
-            </Input>
-          )}
-        />
-        {/* Grade Selection */}
-        <VStack>
-          <Label>{t("Select Grade")}:</Label>
-          <StyledTagsContainer>
-            {Object.values(CLIMBING_GRADE).map((grade) => (
-              <Controller
-                key={grade}
-                name="grade"
-                control={control}
-                render={({ field }) => (
-                  <TouchableOpacity onPress={() => field.onChange(grade)}>
-                    <GradeBadge selected={field.value === grade}>
-                      <BadgeText>{grade}</BadgeText>
-                    </GradeBadge>
-                  </TouchableOpacity>
-                )}
-              />
-            ))}
-          </StyledTagsContainer>
-          {errors.grade && <ErrorText>{errors.grade.message}</ErrorText>}
-        </VStack>
-        {/* Tag Selection */}
-        <VStack>
-          <Label>{t("Select up to 3 tags")}:</Label>
-          <TagsContainer>
-            {Object.values(KILTER_TAGS).map((tag) => (
-              <TouchableOpacity key={tag} onPress={() => toggleTag(tag)}>
-                <TagBadge
-                  style={{
-                    backgroundColor: selectedTags.includes(tag)
-                      ? "#4CAF50"
-                      : "#E0E0E0",
-                  }}
-                >
-                  <BadgeText>{tag}</BadgeText>
-                </TagBadge>
-              </TouchableOpacity>
-            ))}
-          </TagsContainer>
-        </VStack>
-        {/* Status Selection */}
-        <VStack>
-          <Label>{t("Select Status")}:</Label>
-          <TagsContainer>
-            {Object.values(STATUS).map((status) => (
-              <TouchableOpacity
-                key={status}
-                onPress={() => setSelectedStatus(status)}
-              >
-                <StatusBadge
-                  style={{
-                    backgroundColor:
-                      selectedStatus === status
-                        ? status === STATUS.PUBLIC
-                          ? "green"
-                          : status === STATUS.DRAFT
-                          ? "gray"
-                          : "red"
-                        : "#E0E0E0",
-                  }}
-                >
-                  <BadgeText>{t(status)}</BadgeText>
-                </StatusBadge>
-              </TouchableOpacity>
-            ))}
-          </TagsContainer>
-        </VStack>
-        {/* Submit Button */}
-        <VStack style={{ alignItems: "center", width: "100%", marginTop: 40 }}>
-          <Button
-            onPress={handleSubmit(onSubmit, onError)}
-            isDisabled={isSubmitting}
-          >
-            <ButtonText>
-              {isSubmitting ? t("Creating...") : t("Set Boulder")}
-            </ButtonText>
-          </Button>
-        </VStack>
-      </Container>
+            );
+          })}
+        </Svg>
+      </View>
     </ScrollView>
   );
 };
 
-export const CreateClimb = () => {
+export const CreateClimb: React.FC = () => {
   const { t } = useTranslation();
   return (
     <SafeAreaView className="h-full w-full">
